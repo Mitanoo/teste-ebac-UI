5332d126e0ff292fd1839fcb8c4e744661d00c83
Mitanoo <bielbragarose@gmail.com>
2024-03-14 12:47:24 -0300

    Casos de teste negativos e Hooks

--- cypress/e2e/3-Loja-ebac/login.cy.js
+++ cypress/e2e/3-Loja-ebac/login.cy.js
@@ -1,13 +1,34 @@
-/// <reference types="cypress"/>
+///<reference types="cypress"/>
 
 describe=('Funcionalidade: Login', () => {
 
-    it.only('Deve fazer login com sucesso', () => {
+    beforeEach(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        cy.screenshot
+    });
+
+    it('Deve fazer login com sucesso', () => {
         cy.get('#username').type ('teste.incrivel@uau.com.br')
         cy.get('#password').type ('123456789')
         cy.get('.woocomerce-form > .button').click()
-
         cy.get('.woocomerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, teste.incrivel (não é teste.incrivel? Sair)')
-    })  
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+        cy.get('#username').type ('teste.não-incrível')
+        cy.get('#password').type ('123456789')
+        cy.get('.woocomerce-form > .button').click()
+        cy.get('.woocomerce-error').should('exist')
+    });
+
+    it('Deve exibir uma mensagem de erro ao inserir senha inválido', () => {
+        cy.get('#username').type ('teste.incrível@uau.com.br')
+        cy.get('#password').type ('1')
+        cy.get('.woocomerce-form > .button').click()
+        cy.get('.woocomerce-error').should('exist')
+    });
+
 })
